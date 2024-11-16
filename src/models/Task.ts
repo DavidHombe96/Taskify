ca94011273dd7046ebe28697ebb373afc6ce2677
DavidHombe96 <90213056+DavidHombe96@users.noreply.github.com>
2024-11-16 16:14:26 +0100

    add model user and task

---
+++ src/models/Task.ts
@@ -0,0 +1,27 @@
+import  { Schema, model } from "mongoose"
+
+export const Task = model("Task", new Schema({
+	name: {
+		type: String,
+		required: [true, "Name is required"]
+
+	},
+	description: {
+		type: String,
+		required: [true, "Description is required"]
+
+	},
+		user: {
+		type: Schema.Types.ObjectId,
+		ref:"User"
+
+		},
+	createdAt:{
+		type: Date,
+		default: Date.now
+	},
+	updatedAt:{
+		type: Date,
+	}
+
+})) 
