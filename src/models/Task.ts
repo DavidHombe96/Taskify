970bdda6b85d9ce20add480b92e6ffb33acbb3c1
DavidHombe96 <90213056+DavidHombe96@users.noreply.github.com>
2024-11-17 13:55:51 +0100

    update field status

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,19 +3,20 @@ import { Schema, model } from "mongoose"
 export const Task = model("Task", new Schema({
 	name: {
 		type: String,
-		required: [true, "Name is required"]
+		required: [true, "Preencha o campo nome"]
 
 	},
 	description: {
 		type: String,
-		required: [true, "Description is required"]
+		required: [true, "Preencha o campo descrição"]
 
 	},
 	status: {
-		type: Boolean,
-		default: false
-
-	},
+    type:String,
+    required: true,
+    enum: ['Pendente','Concluída'],
+    default:'Pendente',
+  },
 	user: {
 		type: Schema.Types.ObjectId,
 		ref: "User"
