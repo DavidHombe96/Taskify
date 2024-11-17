03b2c7c2fff91428baa67efb20fb6dbf0b6e597b
DavidHombe96 <90213056+DavidHombe96@users.noreply.github.com>
2024-11-17 13:48:34 +0100

    add controller and route for tasks

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from  "express"
 import  {userRouter}  from "./routes/userRoutes"
+import { taskRouter } from "./routes/taskRoutes"
 
 const app =  express()
 
@@ -8,6 +9,7 @@ app.use(express.json())
 const PORT = 50001
 
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tasks", taskRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`)
